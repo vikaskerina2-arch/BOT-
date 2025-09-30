19334dac3b71c88aca7a367501250b311ce9db8c
vikaskerina2-arch <vikaskerina2@gmail.com>
2025-09-30 19:40:20 +0300

    Add files via upload
    BotConfig.java - хранит настройки бота. MyTgBot.java - логика обработки сообщений.

---
+++ main/java/org/example/bot/Main.java
@@ -0,0 +1,40 @@
+package org.example.bot;
+
+public class Main {
+
+    public String processUserInput(String userInput, String userId) {
+        System.out.println("сообщение: " + userInput + " от: " + userId);
+        String outputText = Response(userInput);
+        System.out.println("Ответ: " + outputText);
+        return outputText;
+    }
+
+    private String Response(String userInput){
+        if ("/start".equals(userInput)) {
+            return startMessage();
+        } else if ("/help".equals(userInput)) {
+            return helpMessage();
+        } else {
+            return "Вы ввели: " + userInput;
+        }
+    }
+
+    private String startMessage() {
+        return "Добро пожаловать в наш первый бот!\n" +
+                "Он умеет возвращать текст, который вы ему напишите.\n" +
+                "Просто отправьте ему текстовое сообщение, и он ответит вам.\n" +
+                "Его команды:\n" +
+                "/start - начать работу\n" +
+                "/help - показать, как я работаю\n";
+    }
+
+    private String helpMessage() {
+        return "Справка по работе:\n" +
+                "Бот умеет возвращать текст, который вы ему напишите.\n" +
+                "Например: вы пишите \"привет\", он ответит:\n" +
+                "Вы ввели: \"привет\"\n" +
+                "/start - начать работу\n" +
+                "/help - показать, как я работаю\n" +
+                "Попробуйте ему написать!";
+    }
+}
